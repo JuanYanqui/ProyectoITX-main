96430422f413fe5350bc1b612d30392d1634997e
Slixz0 <90061842+slixz0123@users.noreply.github.com>
2022-12-13 16:48:01 -0500

    creaccion de header log y sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,27 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderlogComponent } from './components/headerlog/headerlog.component';
+
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent,
+    HeaderlogComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],exports:[
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent,
+    HeaderlogComponent
   ]
 })
 export class SharedModule { }
